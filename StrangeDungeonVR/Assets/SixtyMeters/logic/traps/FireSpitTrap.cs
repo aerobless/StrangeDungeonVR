6c3a729b0f48f57ebf0387e0a23727eda47bcce3
Theo Winter <theo.winter@gmail.com>
2022-04-12 22:46:46 +0200

    Started work on a health bar/name tag ui element

---
+++ StrangeDungeonVR/Assets/SixtyMeters/logic/traps/FireSpitTrap.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using SixtyMeters.logic.utilities;
+using UnityEngine;
+
+namespace SixtyMeters.logic.traps
+{
+    public class FireSpitTrap : MonoBehaviour, ITrap
+    {
+        public List<ParticleSystem> fireParticles;
+
+        // Settings
+        public float fireActiveTime;
+
+        // Internals
+        private ParticleSystem _chosenFireEffect;
+        private bool _triggered;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _chosenFireEffect = Helper.GETRandomFromList(fireParticles);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+
+        public void TriggerTrap()
+        {
+            if (!_triggered)
+            {
+                // Wait before spitting, make sound
+                
+                // Spit + sound effect
+                _triggered = true;
+                _chosenFireEffect.gameObject.SetActive(true);
+                _chosenFireEffect.Play();
+                
+                // Reset
+                StartCoroutine(Helper.Wait(fireActiveTime, ResetTrap));
+            }
+        }
+
+        public void ResetTrap()
+        {
+            _triggered = false;
+            _chosenFireEffect.Stop();
+            _chosenFireEffect.gameObject.SetActive(false);
+        }
+    }
+}
